6b1fb74ff125dff3cb33a501e9e36713ecbcc288
vitor11 <vitorsans.057@gmail.com>
2017-10-02 20:49:37 -0300

    Add files via upload

---
+++ Restaurante.java
@@ -0,0 +1,35 @@
+package telalogin.telalogin.object;
+
+/**
+ * Created by vitor on 19/09/2017.
+ */
+public class Restaurante {
+
+    private String nome;
+    private String imagem;
+    private String descricao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
