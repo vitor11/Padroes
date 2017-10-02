b9c127f631fee18bd4a917dc31461a230a59e3e8
vitor11 <vitorsans.057@gmail.com>
2017-10-02 20:57:14 -0300

    Create TelaEscolha.java

---
+++ tela/TelaEscolha.java
@@ -0,0 +1,15 @@
+package telalogin.telas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fatec.telalogin.R;
+
+public class TelaEscolha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_escolha);
+    }
+}
